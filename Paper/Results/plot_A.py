3a0e898266b4921e936bc07f707b0ac4d4d2d549
Swetha Saseendran <68152189+swetha4444@users.noreply.github.com>
2021-09-10 20:40:16 +0530

    Create plot_A.py

---
+++ Paper/Results/plot_A.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# create data
+x = np.arange(4)
+accuracy = [77.6,50.35,47.7,52.06]
+f1 = [83.2,62.1,44.02,60.23]
+precision = [78.1,59.2,66.25,62.33]
+recall = [88.9,65.2,32.96,58.27]
+width = 0.2
+  
+# plot data in grouped manner of bar type
+plt.bar(x-0.2, accuracy, width, color='cyan')
+plt.bar(x, f1, width, color='orange')
+plt.bar(x+0.2, precision, width, color='green')
+plt.bar(x+0.4, recall, width, color='red')
+plt.xticks(x, ['BERT','LSTM','Random Forest','SVC'])
+plt.xlabel("Metrics")
+plt.ylabel("Scores")
+plt.legend(["Accuracy","Macro-F1","Macro-Precision","Macro-Recall"])
+plt.show()
+
